dcb1c9ec0ef70369ca5aa396e021db03dce5ced8
EvaGraceSmith <smithevagrace@gmail.com>
2023-05-26 00:25:51 -0500

    moved model

--- src/server.js
+++ src/server.js
@@ -9,7 +9,9 @@ const cors = require('cors');
 const errorHandler = require('./error-handlers/500.js');
 const notFound = require('./error-handlers/404.js');
 const authRoutes = require('./auth/routes.js');
-const logger = require('./auth/middleware/logger.js');
+const logger = require('./middleware/logger.js');
+const v1Routes = require('./routes/v1.js');
+const v2Routes = require('./routes/v2.js');
 
 // Prepare the express app
 const app = express();
@@ -25,9 +27,11 @@ app.use(logger);
 app.use(authRoutes);
 // REWORK THIS !!!
 // app.use('/api/v1', authRoutes);
+app.use('/api/v1', v1Routes);
+app.use('/api/v2', v2Routes);
 
 // Catchalls
-app.use(notFound);
+app.use('*', notFound);
 app.use(errorHandler);
 
 module.exports = {
